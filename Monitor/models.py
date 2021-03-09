2db1b4bf5a4a22f848dc970dc9589b265d75de9b
Jyootimoy kakoty <jyootimoy.kakoty@gmail.com>
2021-03-09 22:15:57 +0530

    Update on App's models.py, Hospital Models added

--- Monitor/models.py
+++ Monitor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class HospitalData(models.Model):
+    hospitalID = models.TextField(max_length=15, primary_key=True)
+    hospitalName = models.TextField(max_length=30)
+    hospitalAddress =  models.TextField(max_length=50)
+
+ 
--- Monitor/views.py
+++ Monitor/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    hospitals = HospitalData.objects.all()
+    """
+    Index = '<h1>Welcome to ECG-Monitoring System</h1>'
+    for hospital in hospitals:
+        url = '/monitor/' + str(hospital.hospitalID) + '/'
+        Index += '<a href =" ' + url + '">' + hospital.hospitalName + '</a><br>'
+    return HttpResponse(Index)
+    """
+    context = {
+        'hospitals': hospitals,
+    }
+    """
+    template = loader.get_template('monitor/index.html')
+    return HttpResponse(template.render(context, request))
+    """
+    return render(request, 'monitor/index.html', context)
+
