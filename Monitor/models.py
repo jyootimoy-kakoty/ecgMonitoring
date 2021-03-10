756ca861460e880c7e2cf125f8124a547e9a513d
Jyootimoy kakoty <jyootimoy.kakoty@gmail.com>
2021-03-10 08:06:18 +0530

    Update on App's models.py, PatientData, SensorData, ECGData Models added and migrations completed

--- Monitor/models.py
+++ Monitor/models.py
@@ -9,3 +9,27 @@ class HospitalData(models.Model):
 
     def __str__(self):
         return self.hospitalID + ' ' + self.hospitalName + ' ' + self.hospitalAddress
+
+class PatientData(models.Model):
+    patientID = models.TextField(max_length=15, primary_key=True)
+    patientName = models.TextField(max_length=30)
+    hospitalID =  models.ForeignKey(HospitalData, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.patientID + ' ' + self.patientName + ' ' + self.hospitalID.hospitalID
+
+class SensorData(models.Model):
+    sensorID = models.TextField(max_length=15, primary_key=True)
+    sensorName = models.TextField(max_length=15)
+
+    def __str__(self):
+        return self.sensorID + ' ' + self.sensorName
+
+class ECGData(models.Model):
+    sensorID = models.ForeignKey(SensorData, on_delete=models.CASCADE)
+    patientID = models.ForeignKey(PatientData, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    data = models.FloatField()
+
+    def __str__(self):
+        return self.sensorID.sensorID + ' ' + self.patientID.patientID + ' ' + str(self.time) + ' ' + str(self.data)
